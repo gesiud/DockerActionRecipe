d78430adb2fd45c5505828ca05f78e19d7f86067
Grzegorz Dabrowski <3940032+gesiud@users.noreply.github.com>
2025-12-17 18:47:16 +0100

    Fail CI build

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Witaj, $@!!!!"
 
-echo "answer=42" >> $GITHUB_OUTPUT
+echo "answer=17" >> $GITHUB_OUTPUT
 
 echo "### Hello $@! :rocket: " >> $GITHUB_STEP_SUMMARY
 echo "<h3>The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
